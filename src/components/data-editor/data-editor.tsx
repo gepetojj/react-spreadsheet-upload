e3963b8d4d670eb18e34828ea0905dcaff906f5f
João Pedro <nmjoaopedro22@gmail.com>
2025-09-22 14:00:47 -0300

    refactor: torna todos os componentes use client para aprimorar integração

--- src/components/data-editor/data-editor.tsx
+++ src/components/data-editor/data-editor.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import clsx from "clsx";
 import type React from "react";
 import { useCallback, useEffect, useMemo, useRef, useState } from "react";
--- src/components/validation/validation.tsx
+++ src/components/validation/validation.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import clsx from "clsx";
 import { useMemo } from "react";
 
